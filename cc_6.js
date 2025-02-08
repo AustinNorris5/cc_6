d20a1fff243caa099af748ffe3e8985d03d1e5da
AustinNorris5 <ajnorris@usf.edu>
2025-02-08 15:39:21 -0500

    Task 8 - Employee Promotion Evaluation

--- cc_6.js
+++ cc_6.js
@@ -100,3 +100,17 @@ function createExpenseTracker() {
 let tracker = createExpenseTracker();
 console.log(tracker(200));
 console.log(tracker(150));
+
+//Task 8: Recursion in JavaScript
+
+//Write a recursive function that determines years to reach level 10
+function calculateYearsToPromotion(employeeLevel) {
+    if (employeeLevel >= 10) {
+        return 0;
+    };
+    return 2 + calculateYearsToPromotion(employeeLevel +1);
+};
+
+//Log function to the console and test data
+console.log(`Years to Level 10: ${calculateYearsToPromotion(7)}`);
+console.log(`Years to Level 10: ${calculateYearsToPromotion(5)}`);
