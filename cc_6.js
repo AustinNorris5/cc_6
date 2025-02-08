3213a55ae9ad04ae0ef766020927241e1ac42164
AustinNorris5 <ajnorris@usf.edu>
2025-02-08 14:53:30 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -22,3 +22,21 @@ let calculateSalesTax = function(amount, taxRate) {
 console.log(calculateSalesTax(100, 0.07));
 console.log(calculateSalesTax(500, 0.1));
 
+//Task 3: Arrow Function
+
+//Write an arrow function to detrmine a bonus
+let calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage =
+        performanceRating = "Excellent"
+        if (bonusPercentage = 0.2);
+        performanceRating = "Good"
+        if (bonusPercentage = 0.1);
+        performanceRating = "Average"
+        if (bonusPercentage = 0.05);
+        let bonus = salary * bonusPercentage;
+        return (`Bonus: $${bonus}`);
+};
+
+//Log function to the console
+console.log(calculateBonus(5000, "Excellent"));
+console.log(calculateBonus(7000, "Good"))
